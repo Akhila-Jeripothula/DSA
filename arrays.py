9de79d41bf63f4e18e090ec3bf9436225b81bdb5
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-07-27 19:40:20 +0530

    push

--- arrays.py
+++ arrays.py
@@ -496,9 +496,18 @@
 
 # MISSING NUMBER :
 
-nums=[3,0,1,4]
-n=len(nums)
-expected_sum=n*(n+1)//2
-actual_sum=sum(nums)
-missing=expected_sum-actual_sum
-print(missing)
+#1.
+
+# nums=[3,0,1,4]
+# n=len(nums)
+# expected_sum=n*(n+1)//2
+# actual_sum=sum(nums)
+# missing=expected_sum-actual_sum
+# print(missing)
+
+
+
+#2.
+nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+
+

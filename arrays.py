3a8342d892b5bf65b858a1513b76ef93b320657a
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-07-30 19:12:02 +0530

    contain Duplicates

--- arrays.py
+++ arrays.py
@@ -643,3 +643,11 @@
 
 
         
+
+# TOPIC : CONTAINS DUPLICATE
+nums=[1,2,3,1]
+for i in range(len(nums)):
+    for j in range(i+1,len(nums)):
+        if nums[i]==nums[j]:
+            print(True)
+print(False)

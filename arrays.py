c7636988d1b94983596ba1a143d364f640df99a2
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-07-30 12:40:25 +0530

    twosum

--- arrays.py
+++ arrays.py
@@ -631,5 +631,15 @@
 
 
 
+#optimal approach:
+# nums=[3,2,4]
+# target=6
+# seen={}
+# for i in range(len(nums)):
+#     complement=target-nums[i]
+#     if complement in seen:
+#         print([seen[complement],i])
+#     seen[nums[i]]=i
+
 
         

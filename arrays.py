5950445c153b107633251a4ab0055863dcba6c39
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-08-01 09:55:47 +0530

    product of array except self - optimal

--- arrays.py
+++ arrays.py
@@ -683,16 +683,16 @@
 
 #optimal solution(Using prefix sum):
 
-nums=[1,2,3,4]
-n=len(nums)
-prefix=[1]*n
-suffix=[1]*n
-answer=[1]*n
-for i in range(1,n):
-    prefix[i]=prefix[i-1]*nums[i-1]
-for i in range(n-2,-1,-1):
-    suffix[i]=suffix[i+1]*nums[i+1]
-for i in range(n):
-    answer[i]=prefix[i]*suffix[i]
-print(answer)
+# nums=[1,2,3,4]
+# n=len(nums)
+# prefix=[1]*n
+# suffix=[1]*n
+# answer=[1]*n
+# for i in range(1,n):
+#     prefix[i]=prefix[i-1]*nums[i-1]
+# for i in range(n-2,-1,-1):
+#     suffix[i]=suffix[i+1]*nums[i+1]
+# for i in range(n):
+#     answer[i]=prefix[i]*suffix[i]
+# print(answer)
 

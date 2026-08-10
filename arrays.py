dbc0efc24b1b8522b35bd87b5b8ba465b9e5b265
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-08-10 15:11:48 +0530

    kadanes

--- arrays.py
+++ arrays.py
@@ -738,4 +738,26 @@
 # print(secondlargest)
 
 
-#maxsubarray(kadanae algorithm):
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Arrays Continuation:
+# KADANE'S ALGORITHM(MAXIMUM SUB ARRAY):
+
+nums=[-2,1,-3,4,-1,2,1,-5,4]
+current_sum=nums[0]
+max_sum=nums[0]
+for i in range(1,len(nums)):
+    current_sum=max(nums[i],nums[i]+current_sum)
+    max_sum=max(current_sum,max_sum)
+print(max_sum)

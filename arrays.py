6717df84785f18c3f180ac7a368e99ced298dbb1
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-09-10 21:05:05 +0530

    push

--- arrays.py
+++ arrays.py
@@ -826,6 +826,8 @@
 
 
 
+###########################
+#####################
 
 #new topic : container wiith most water
 
@@ -844,5 +846,24 @@
 #             else:
 #                 right-=1
 #         return max_area
-        
+
+
+
+
+
+
+
+
+# New Topic : Maximum consecutive Ones:
+
+nums=[1,0,1,1,1]
+count=0
+max_count=0
+for num in nums:
+    if num==1:
+        count+=1
+        max_count=max(count,max_count)
+    else:
+        count=0
+print(max_count)        
 

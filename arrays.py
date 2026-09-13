2bc1f73ccd5ea722aac8d512e14ab365ad2329d5
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-09-13 14:47:39 +0530

    3sum

--- arrays.py
+++ arrays.py
@@ -869,8 +869,49 @@
 #find duplicates
 
 #brute force:
-nums=[1,2,3,1]
-for i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-        if(nums[i]==nums[j]):
-            print(nums[i])
+# nums=[1,2,3,1]
+# for i in range(len(nums)):
+#     for j in range(i+1,len(nums)):
+#         if(nums[i]==nums[j]):
+#             print(nums[i])
+
+    
+
+
+#3 sum:
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        answer = []
+
+        for i in range(len(nums) - 2):
+
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+
+                elif total > 0:
+                    right -= 1
+
+                else:
+                    answer.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return answer

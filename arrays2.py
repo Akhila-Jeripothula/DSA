db5d400c924e722bfa98044c75cc6ab626d0aa02
Akhila-Jeripothula <akhilajeripothula075@gmail.com>
2026-09-23 22:36:06 +0530

    leetcode2

--- arrays2.py
+++ arrays2.py
@@ -84,20 +84,67 @@
 
 
 #optimal:
-class Solution:
-    def longestConsecutive(self, nums):
-        num_set = set(nums)
-        longest = 0
+# class Solution:
+#     def longestConsecutive(self, nums):
+#         num_set = set(nums)
+#         longest = 0
 
-        for num in num_set:
-            if num - 1 not in num_set:
-                current = num
-                count = 1
+#         for num in num_set:
+#             if num - 1 not in num_set:
+#                 current = num
+#                 count = 1
 
-                while current + 1 in num_set:
-                    current += 1
-                    count += 1
+#                 while current + 1 in num_set:
+#                     current += 1
+#                     count += 1
 
-                longest = max(longest, count)
+#                 longest = max(longest, count)
 
-        return longest
+#         return longest
+
+
+
+
+
+
+#leetcode 2 :
+# class Solution:
+#     def addTwoNumbers(self, l1, l2):
+
+#         # Convert first linked list into a number
+#         num1 = 0
+#         place = 1
+
+#         while l1:
+#             num1 = num1 + l1.val * place
+#             place = place * 10
+#             l1 = l1.next
+
+#         # Convert second linked list into a number
+#         num2 = 0
+#         place = 1
+
+#         while l2:
+#             num2 = num2 + l2.val * place
+#             place = place * 10
+#             l2 = l2.next
+
+#         # Add the two numbers
+#         total = num1 + num2
+
+#         # Convert the result back into a linked list
+#         dummy = ListNode(0)
+#         current = dummy
+
+#         if total == 0:
+#             return dummy.next
+
+#         while total > 0:
+#             digit = total % 10
+
+#             current.next = ListNode(digit)
+#             current = current.next
+
+#             total = total // 10
+
+#         return dummy.next
